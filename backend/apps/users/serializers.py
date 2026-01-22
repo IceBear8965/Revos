bec75b95528a282d9a89c5c15784466edf6df376
IceBear8965 <icebee088@gmail.com>
2026-01-22 13:10:25 +0100

    Register API finished
    Register API with energy choices finished

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 from apps.energy.constants import LOAD_ACTIVITIES
 from apps.energy.models import EnergyProfile, PersonalActivityProfile
 
+from .constants import INITIAL_ENERGY_CHOICES, INITIAL_ENERGY_MAP
 from .models import User
 
 
@@ -12,9 +13,10 @@ class RegisterUserSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(max_length=16)
     nickname = serializers.CharField(max_length=32)
-    current_energy = serializers.FloatField(min_value=0.05, max_value=1.0)
     timezone = serializers.CharField(max_length=32)
     load_order = serializers.ListField(child=serializers.ChoiceField(choices=LOAD_ACTIVITIES))
+
+    initial_energy_state = serializers.ChoiceField(choices=INITIAL_ENERGY_CHOICES)
 
     def validate(self, data):
         if set(data.get("load_order")) != set(LOAD_ACTIVITIES):
@@ -41,7 +43,9 @@ class RegisterUserSerializer(serializers.Serializer):
             user.set_password(validated_data["password"])
             user.save()
 
-            EnergyProfile.objects.create(user=user, current_energy=validated_data["current_energy"])
+            current_energy = INITIAL_ENERGY_MAP[validated_data["initial_energy_state"]]
+
+            EnergyProfile.objects.create(user=user, current_energy=current_energy)
 
             PersonalActivityProfile.objects.create(
                 user=user, load_order=validated_data["load_order"]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .serializers import RegisterUserSerializer
 
@@ -13,6 +14,14 @@ class RegisterUserView(APIView):
     def post(self, request):
         serializer = RegisterUserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        validated_data = serializer.validated_data
         user = serializer.save()
-        return Response(validated_data, status=HTTP_201_CREATED)
+
+        refresh = RefreshToken.for_user(user)
+        access = refresh.access_token
+        return Response(
+            {
+                "refresh": str(refresh),
+                "access": str(access),
+            },
+            status=HTTP_201_CREATED,
+        )
