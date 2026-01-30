a7ae2d3d0888adee86362f7e817f1701654e326b
IceBear8965 <icebee088@gmail.com>
2026-01-30 19:36:19 +0100

    Home page layout finished

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,44 +1,13 @@
 import { useTheme } from "@/context/ThemeContext"
-import { Text, View, StyleSheet, Image, TouchableWithoutFeedback } from "react-native"
+import { Text, View, Image, TouchableWithoutFeedback } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
+import { createStyles } from "@/styles/index"
+import Feather from "@expo/vector-icons/Feather"
 
 export default function Index() {
-    const { colors, toggleTheme } = useTheme()
+    const { colors } = useTheme()
 
-    const styles = StyleSheet.create({
-        screen: {
-            flex: 1,
-        },
-        screenTop: {
-            flex: 4,
-            backgroundColor: colors.foreground,
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20,
-        },
-        screenBottom: {
-            flex: 6,
-        },
-
-        header: {
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-            paddingHorizontal: 20,
-            paddingVertical: 12,
-            backgroundColor: colors.topBar,
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20,
-        },
-        greeting: {
-            fontSize: 22,
-            fontWeight: "600",
-            color: colors.textSecondary,
-        },
-        userIcon: {
-            width: 45,
-            height: 45,
-        },
-    })
+    const styles = createStyles(colors)
 
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: colors.background }}>
@@ -54,7 +23,31 @@ export default function Index() {
                         </TouchableWithoutFeedback>
                     </View>
                 </View>
-                <View style={styles.screenBottom}></View>
+                <View style={styles.screenBottom}>
+                    <View style={styles.messageCard}>
+                        <Text style={styles.messageTitle}>Message Title</Text>
+                        <Text style={styles.messageContent}>Message Content</Text>
+                    </View>
+                    <View style={styles.recommendationCard}>
+                        <Text style={styles.recommendationText}>Recommendation</Text>
+                    </View>
+                    <View style={styles.lastEventCard}>
+                        <View style={styles.lastEventLeft}>
+                            <Text style={styles.lastEventType}>Activity Type</Text>
+                            <View style={styles.lastEventDateTime}>
+                                <Text style={styles.lastEventDate}>01.01.2026</Text>
+                                <Text style={styles.lastEventTime}>11:00-12:30</Text>
+                            </View>
+                        </View>
+                        <View style={styles.lastEventRight}>
+                            <View style={styles.arrowsContainer}>
+                                <Feather name="arrow-down" size={30} color={colors.accentRed} />
+                                <Feather name="arrow-down" size={30} color={colors.accentRed} />
+                                <Feather name="arrow-down" size={30} color={colors.accentRed} />
+                            </View>
+                        </View>
+                    </View>
+                </View>
             </View>
         </SafeAreaView>
     )
---
+++ frontend/styles/index.ts
@@ -0,0 +1,114 @@
+import { AppColors } from "@/theme/colors"
+import { StyleSheet } from "react-native"
+
+export const createStyles = (colors: AppColors) => {
+    return StyleSheet.create({
+        screen: {
+            flex: 1,
+            paddingBottom: 90,
+        },
+        screenTop: {
+            flex: 5,
+            backgroundColor: colors.foreground,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+            marginBottom: 20,
+        },
+
+        header: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 20,
+            paddingVertical: 12,
+            backgroundColor: colors.topBar,
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+        },
+        greeting: {
+            fontSize: 22,
+            fontWeight: "600",
+            color: colors.textSecondary,
+        },
+        userIcon: {
+            width: 45,
+            height: 45,
+        },
+
+        screenBottom: {
+            flex: 5,
+            marginHorizontal: 15,
+            rowGap: 15,
+        },
+
+        messageCard: {
+            flex: 3,
+            padding: 20,
+            backgroundColor: colors.card,
+            borderRadius: 30,
+        },
+        messageTitle: {
+            color: colors.textPrimary,
+            fontSize: 24,
+            fontWeight: 600,
+            marginBottom: 10,
+        },
+        messageContent: {
+            color: colors.textPrimary,
+            fontSize: 16,
+            fontWeight: 400,
+        },
+
+        recommendationCard: {
+            flex: 2,
+            backgroundColor: colors.card,
+            borderRadius: 30,
+            justifyContent: "center",
+            paddingHorizontal: 20,
+        },
+        recommendationText: {
+            fontSize: 16,
+            fontWeight: 400,
+            color: colors.textPrimary,
+        },
+
+        lastEventCard: {
+            flex: 3,
+            padding: 20,
+            backgroundColor: colors.card,
+            borderRadius: 30,
+
+            flexDirection: "row",
+            justifyContent: "space-between",
+        },
+
+        lastEventLeft: {
+            flex: 4,
+        },
+        lastEventType: {
+            color: colors.textPrimary,
+            fontSize: 24,
+            fontWeight: 600,
+
+            marginBottom: 10,
+        },
+        lastEventDateTime: {},
+        lastEventDate: {
+            color: colors.textPrimary,
+            fontSize: 16,
+            fontWeight: 400,
+        },
+        lastEventTime: {
+            color: colors.textPrimary,
+            fontSize: 16,
+            fontWeight: 400,
+        },
+
+        lastEventRight: {
+            justifyContent: "center",
+        },
+        arrowsContainer: {
+            flexDirection: "row",
+        },
+    })
+}
