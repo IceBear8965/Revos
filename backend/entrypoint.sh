d87bd406cc4d660fa3fca24902e8e22d79622a28
IceBear8965 <icebee088@gmail.com>
2026-05-07 10:43:12 +0200

    Ready for deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
+set -e
+
 echo "Waiting for postgres..."
 
-while ! nc -z db 5432; do
+DB_HOST=$(python -c "from urllib.parse import urlparse; import os; print(urlparse(os.environ['DATABASE_URL']).hostname)")
+DB_PORT=$(python -c "from urllib.parse import urlparse; import os; print(urlparse(os.environ['DATABASE_URL']).port)")
+
+while ! nc -z $DB_HOST $DB_PORT; do
   sleep 1
 done
 
 echo "PostgreSQL started"
 
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-# python manage.py runserver 0.0.0.0:8000
-exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+exec gunicorn config.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8000}
